87aab928a377d008f88ce0ff75f89188aae92069
Kamal Nayan Singh <102502739+kamalnyan@users.noreply.github.com>
2024-09-13 10:19:32 +0530

    class

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         brightness: Brightness.dark,
         primaryColor: Colors.black,
       ),
-      home:  const otp_screen(),
+      home:   MyHomePage(),
     );
   }
 }
